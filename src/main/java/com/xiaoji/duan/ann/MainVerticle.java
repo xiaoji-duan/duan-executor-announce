c45876c178fbe98858d0f99d74d666cde74ad590
leon_xi <leon_xi@163.com>
2019-11-15 11:01:03 +0800

    增加中英文字符长度判断

--- src/main/java/com/xiaoji/duan/ann/MainVerticle.java
+++ src/main/java/com/xiaoji/duan/ann/MainVerticle.java
@@ -167,7 +167,7 @@ public class MainVerticle extends AbstractVerticle {
 		} else {
 			String content = payload.getString("content");
 			
-			if (content.length() > 18) {
+			if (length(content) > 36) {
 				// 使用大文字样式
 		        return PushPayload.newBuilder()
 		        		.setPlatform(Platform.android())
@@ -712,4 +712,28 @@ public class MainVerticle extends AbstractVerticle {
 		});
 		
 	}
+	
+	public static boolean isLetter(char c) {   
+       int k = 0x80;   
+       return c / k == 0 ? true : false;   
+   }  
+	
+	/**  
+     * 得到一个字符串的长度,显示的长度,一个汉字或日韩文长度为2,英文字符长度为1  
+     * @param String s 需要得到长度的字符串  
+     * @return int 得到的字符串长度  
+     */   
+   public static int length(String s) {  
+       if (s == null)  
+           return 0;  
+       char[] c = s.toCharArray();  
+       int len = 0;  
+       for (int i = 0; i < c.length; i++) {  
+           len++;  
+           if (!isLetter(c[i])) {  
+               len++;  
+           }  
+       }  
+       return len;  
+   }  
 }
